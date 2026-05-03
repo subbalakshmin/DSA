3dce693066cf93675792c9c5cfca4463c04f64ba
subbalakshmin <subbalakshminarasimhan@gmail.com>
2026-05-03 06:51:37 +0530

    Update CenteredTriangle.java

--- Patterns/CenteredTriangle.java
+++ Patterns/CenteredTriangle.java
@@ -4,7 +4,7 @@ public class CenteredTriangle {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=n-i;j++){
+            for(int j=1;j<=n-i;j++){                                                 
                 System.out.print(" ");
             }
             for(int j=0;j<2*i-1;j++){
